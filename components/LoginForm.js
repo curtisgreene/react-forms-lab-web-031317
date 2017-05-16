9ffa98322b71cfc27f9024f3bd01a2b9e00e2678
curtisgreene <curtis.j.w.greene@gmail.com>
2017-05-16 18:40:37 -0400

    Done.

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -4,22 +4,44 @@ export default class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
+
+  handleUsername(event) {
+    this.setState({
+      username: event.target.value
+    })
+  }
+
+  handlePassword(event) {
+    this.setState({
+      password: event.target.value
+    })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+    if (this.state.username.length > 0 && this.state.password.length > 0) {
+      this.props.onSubmit()
+     }
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit.bind(this)}>
         <div>
           <label>
             Username
-            <input id="test-username" type="text" />
+            <input id="test-username" value={this.state.username} type="text" onChange={this.handleUsername.bind(this)} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="test-password" type="password" />
+            <input id="test-password" value={this.state.passward} type="password" onChange={this.handlePassword.bind(this)} />
           </label>
         </div>
         <div>
--- components/TwitterMessage.js
+++ components/TwitterMessage.js
@@ -4,14 +4,28 @@ export default class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      value: ''
+    };
   }
+
+  handleTyping(event){
+    this.setState({
+      value: event.target.value
+    })
+  }
+  //
+  // remainingCharacters(){
+  //   const max = this.props.maxChars
+  //   return max - this.state.value.length
+  // }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input type="text" value={this.state.value} onChange={this.handleTyping.bind(this)} />
+        <strong>Remaining characters: {this.props.maxChars - this.state.value.length}</strong>
       </div>
     );
   }
